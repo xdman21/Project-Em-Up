ba8b8900b0400b4248e967b6d9fd055bae91a110
xavi <xavier.flores@enti.cat>
2020-03-03 19:33:14 +0100

    Nova escena amb player cotroller
    afegit moviment 2.5d al jugador en una escena simple

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) && this.transform.position.y < 1.158488f) 
+		{
+			this.transform.Translate(new Vector3(0,1,1.2f) * Time.deltaTime * speed);
+		}
+		if(Input.GetKey(KeyCode.A)) 
+		{
+			this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+		}
+		if(Input.GetKey(KeyCode.S) && this.transform.position.y > -2.86f) 
+		{
+			this.transform.Translate(new Vector3(0,-1,-1.2f) * Time.deltaTime * speed);
+		}
+		if(Input.GetKey(KeyCode.D)) 
+		{
+			this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+		}
+    }
+}
