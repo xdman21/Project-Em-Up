6461005dc43cddb0942737f99e21f6df4eb27928
xavi <xavier.flores@enti.cat>
2020-03-04 19:18:32 +0100

    afegint animacions del jugador amb estat idle/moviment

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float speed = 2;
+	public GameObject playerIdle;
+	public GameObject playerMove;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +24,29 @@ public class playerController : MonoBehaviour
 		if(Input.GetKey(KeyCode.A)) 
 		{
 			this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+			playerIdle.transform.rotation = new Quaternion(0,0,0,0);
+			playerMove.transform.rotation = new Quaternion(0,0,0,0);
 		}
 		if(Input.GetKey(KeyCode.S) && this.transform.position.y > -3.0f) 
 		{
 			this.transform.Translate(new Vector3(0,-1,-1.2f) * Time.deltaTime * speed);
+			
 		}
 		if(Input.GetKey(KeyCode.D)) 
 		{
 			this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+			playerIdle.transform.rotation = new Quaternion(0,180,0,0);
+			playerMove.transform.rotation = new Quaternion(0,180,0,0);
+		}
+		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+		{
+			playerIdle.SetActive(false);
+			playerMove.SetActive(true);
+		}
+		else 
+		{
+			playerIdle.SetActive(true);
+			playerMove.SetActive(false);
 		}
     }
 }
