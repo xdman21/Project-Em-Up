1c1bded1ebff9933bd719248c7f0e8c104b68ada
Marc Ramis <marc.ramis@enti.cat>
2020-03-04 19:59:25 +0100

    Player collision screen, primera versión.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public float speed = 2;
+    public Camera Cam2d;
 	public GameObject playerIdle;
 	public GameObject playerMove;
 
@@ -17,36 +18,56 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) && this.transform.position.y < -1f) 
-		{
-			this.transform.Translate(new Vector3(0,1,1.2f) * Time.deltaTime * speed);
-		}
-		if(Input.GetKey(KeyCode.A)) 
-		{
-			this.transform.Translate(Vector3.left * Time.deltaTime * speed);
-			playerIdle.transform.rotation = new Quaternion(0,0,0,0);
-			playerMove.transform.rotation = new Quaternion(0,0,0,0);
-		}
-		if(Input.GetKey(KeyCode.S) && this.transform.position.y > -3.0f) 
-		{
-			this.transform.Translate(new Vector3(0,-1,-1.2f) * Time.deltaTime * speed);
-			
-		}
-		if(Input.GetKey(KeyCode.D)) 
-		{
-			this.transform.Translate(Vector3.right * Time.deltaTime * speed);
-			playerIdle.transform.rotation = new Quaternion(0,180,0,0);
-			playerMove.transform.rotation = new Quaternion(0,180,0,0);
-		}
-		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-		{
-			playerIdle.SetActive(false);
-			playerMove.SetActive(true);
-		}
-		else 
-		{
-			playerIdle.SetActive(true);
-			playerMove.SetActive(false);
-		}
+        playerMovement();
     }
+
+    void playerMovement()
+    {
+        if (Input.GetKey(KeyCode.W) && this.transform.position.y < -1f)
+        {
+            this.transform.Translate(new Vector3(0, 1, 1.2f) * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A) && Cam2d.WorldToScreenPoint(this.transform.position).x > 20)
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime * speed);
+            //SPRITE ROTATION LEFT
+            playerIdle.transform.rotation = new Quaternion(0, 0, 0, 0);
+            playerMove.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && this.transform.position.y > -3.0f)
+        {
+            this.transform.Translate(new Vector3(0, -1, -1.2f) * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.D) && Cam2d.WorldToScreenPoint(this.transform.position).x < 950)
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+            //SPRITE ROTATION RIGHT
+            playerIdle.transform.rotation = new Quaternion(0, 180, 0, 0);
+            playerMove.transform.rotation = new Quaternion(0, 180, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 3;
+        }
+        else
+        {
+            speed = 2;
+        }
+            
+
+        //ANIMATION
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            playerIdle.SetActive(false);
+            playerMove.SetActive(true);
+        }
+        else
+        {
+            playerIdle.SetActive(true);
+            playerMove.SetActive(false);
+        }
+    }
+
 }
